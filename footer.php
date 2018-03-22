9672b9b3f4640aa8d01970c453d92060c970642a
Mike Alberghini <michael.alberghini@keysight.com>
2018-03-22 18:14:14 -0400

    Change filename to fit correct case

--- footer.php
+++ footer.php
@@ -38,7 +38,7 @@
 <audio src="assets/audio/notice2.ogg" id="successSound"></audio>
 <audio src="assets/audio/buzz-two.ogg" id="duplicateSound"></audio>
 <script>var ticket_length = <?php echo TICKET_LENGTH; ?>;</script>
-<script src="assets/js/jquery.js"></script>
+<script src="assets/js/jQuery.js"></script>
 <script src="assets/js/bootstrap.js"></script>
 <script src="assets/js/app.js"></script>
 </body>
